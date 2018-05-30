8ba8260491385f873fc1c59e2aa530a839d7b9ef
Elana Ryznar <eryzna@gmail.com>
2018-05-30 22:10:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,3 +13,7 @@ class Anagram
 end
 
     #w_array.find_all {|x| x==word.split("").sort.join("")}
+
+    #def match(array)
+        #array.select {|x| x.split("").sort == @name.split("").sort}
+    #end   
