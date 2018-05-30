9a8c07487cc01ef2cebf410fb278398960602524
Elana Ryznar <eryzna@gmail.com>
2018-05-30 22:08:20 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,9 @@ class Anagram
   end
   def match(array)
       array.select {|x| x.split("").sort == @name.split("").sort}
-    end   
+  end   
   #def match(array)
-    
+
     #w_array=array.map {|w| w.split("")}
     #matches=w_array.find_all {|x| x.sort==word.split("").sort}
     #matches.map {|y| y.join("")}
