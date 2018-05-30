245260b6aa236e3deb233bb544b1a8b56218d20c
Elana Ryznar <eryzna@gmail.com>
2018-05-30 22:08:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,14 @@ class Anagram
   def initialize(word)
     @word=word
   end
-
   def match(array)
-    w_array=array.map {|w| w.split("")}
-    matches=w_array.find_all {|x| x.sort==word.split("").sort}
-    matches.map {|y| y.join("")}
+      array.select {|x| x.split("").sort == @name.split("").sort}
+    end   
+  #def match(array)
+    
+    #w_array=array.map {|w| w.split("")}
+    #matches=w_array.find_all {|x| x.sort==word.split("").sort}
+    #matches.map {|y| y.join("")}
   end#.sort.join("")}
 end
 
